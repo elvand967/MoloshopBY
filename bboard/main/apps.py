124dfc4e26f43fcac0a2da2662a801712753b7d2
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-02-14 21:23:41 +0300

    main application setup

--- bboard/main/apps.py
+++ bboard/main/apps.py
@@ -1,6 +1,9 @@
+# D:\User\Documents\PycharmProjects\MoloshopBY\bboard\main\apps.py
+
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    verbose_name = 'Доска объявлений'
